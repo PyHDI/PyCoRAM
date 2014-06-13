cf03665df8cd44f5148ce5c29e857b9ef8bc04e2
shtaxxx <shta.ky1018@gmail.com>
2014-06-13 10:28:34 +0900

    A testbench of cache and its related template are updated.

--- input/cache/testbench.v
+++ input/cache/testbench.v
@@ -16,69 +16,49 @@ initial begin
   _re = 0;
   _we = 0;
   _addr = 0;
-  wait(stall == 0);
-  nclk();
+  wait(~URESETN);
+  wait(URESETN);
+  
+  nclk(); while(stall) nclk();
 
   for(a=0; a<16; a=a+1) begin
     _addr = a << 2;
     _we = 0;
     _re = 1;
-    nclk();
-    wait(stall == 0);
-    #1;
+    nclk(); while(stall) nclk();
     $display("# read  addr=%x, q=%x", addr, q);
     sum = q;
     
     _addr = (a << 2) + 'h10000;
     _we = 0;
     _re = 1;
-    nclk();
-    wait(stall == 0);
-    #1;
+    nclk(); while(stall) nclk();
     $display("# read  addr=%x, q=%x", addr, q);
     sum = sum + q;
-
-/* -----\/----- EXCLUDED -----\/-----
-    _addr = (a << 2) + 'h20000;
-    _re = 1;
-    _we = 0;
-    nclk();
-    wait(stall == 0);
-    #1;
-    $display("# read  addr=%x, q=%x", addr, q);
- -----/\----- EXCLUDED -----/\----- */
 
     _addr = (a << 2) + 'h20000;
     _re = 0;
     _we = 1;
     _d = sum;
+    nclk(); while(stall) nclk();
     $display("# write addr=%x, d=%x", addr, d);
-    nclk();
-    wait(stall == 0);
-    #1;
-
+    
     _addr = (a << 2) + 'h30000;
     _we = 0;
     _re = 1;
-    nclk();
-    wait(stall == 0);
-    #1;
+    nclk(); while(stall) nclk();
     $display("# read  addr=%x, q=%x", addr, q);
 
     _addr = (a << 2) + 'h40000;
     _we = 0;
     _re = 1;
-    nclk();
-    wait(stall == 0);
-    #1;
+    nclk(); while(stall) nclk();
     $display("# read  addr=%x, q=%x", addr, q);
 
     _addr = (a << 2) + 'h20000;
     _re = 1;
     _we = 0;
-    nclk();
-    wait(stall == 0);
-    #1;
+    nclk(); while(stall) nclk();
     $display("# read  addr=%x, q=%x", addr, q);
     if(sum == q) $display("# OK sum=%x q=%x", sum, q);
     else $display("# NG sum=%x q=%x", sum, q);
