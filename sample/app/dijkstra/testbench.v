ba12ece278b49881873038f59f9c6af06bf7a225
shtaxxx <shta.ky1018@gmail.com>
2015-05-22 16:24:35 +0900

    iochannel

--- sample/app/dijkstra/testbench.v
+++ sample/app/dijkstra/testbench.v
@@ -1,31 +1,27 @@
-//parameter START_ID = 1826;
-//parameter GOAL_ID = 217;
-//parameter START_ADDR = 'h41630; // ID 217
-//parameter GOAL_ADDR  = 'h45a30; // ID 1826
+parameter START_ID = 1826;
+parameter GOAL_ID = 217;
 
 //parameter START_ID = 1056;
 //parameter GOAL_ID = 674;
-//parameter START_ADDR = 'h40000; // ID 1056
-//parameter GOAL_ADDR  = 'h40010; // ID 674
 
-parameter START_ID = 0;
-parameter GOAL_ID = 1;
+//parameter START_ID = 0;
+//parameter GOAL_ID = 1;
 
+/* -----\/----- EXCLUDED -----\/-----
 // 1MB (default)
 parameter PAGE_OFFSET = 'h000100;
 parameter NODE_OFFSET = 'h040000;
 parameter IDTB_OFFSET = 'h080000;
 parameter ADTB_OFFSET = 'h090000;
 parameter HEAP_OFFSET = 'h100000;
+ -----/\----- EXCLUDED -----/\----- */
 
-/* -----\/----- EXCLUDED -----\/-----
 // 128MB
 parameter PAGE_OFFSET = 'h0000100;
 parameter NODE_OFFSET = 'h6000000;
 parameter IDTB_OFFSET = 'h7000000;
 parameter ADTB_OFFSET = 'h7400000;
 parameter HEAP_OFFSET = 'h7800000;
- -----/\----- EXCLUDED -----/\----- */
 
 parameter DATA_WIDTH = 32;
 
@@ -75,8 +71,10 @@ initial begin
   iochannel_write_cthread_dijkstra_coramiochannel_0(HEAP_OFFSET, 'h0); // heap offset
   iochannel_write_cthread_dijkstra_coramiochannel_0(start_addr, 'h4); // start_node_addr
   iochannel_write_cthread_dijkstra_coramiochannel_0(goal_addr, 'h8); // goal_node_addr
+
   iochannel_read_cthread_dijkstra_coramiochannel_0(cost,'h100); // get result
   iochannel_read_cthread_dijkstra_coramiochannel_0(cycles,'h100); // get cycle count
+  
   $display("# cost = %d", cost);
   $display("# cycles = %d", cycles);
   
